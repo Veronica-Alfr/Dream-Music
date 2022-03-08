16b8f2162163a93bd6708cad234f5d6831f84921
Veronica-Alfr <veagalves@gmail.com>
2022-03-08 13:29:35 -0300

    Requisito 5 finalizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artist: '',
+      disabledButtonSearch: true,
+    };
+  }
+
+  inputArtistChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    },
+    () => this.validationNameArtist());
+  }
+
+  validationNameArtist = () => {
+    const { artist } = this.state;
+    const minWords = 2;
+    this.setState({ disabledButtonSearch: artist.length < minWords });
+  };
+
   render() {
+    const { artist, disabledButtonSearch } = this.state;
     return (
       <section>
         <Header />
         <div data-testid="page-search" />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="artist"
+            value={ artist }
+            onChange={ this.inputArtistChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            name="btn"
+            disabled={ disabledButtonSearch }
+            onClick={ this.buttonSearchArtist }
+          >
+            Pesquisar
+          </button>
+        </form>
       </section>
     );
   }
