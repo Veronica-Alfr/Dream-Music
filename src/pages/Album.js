7edb6966806f4fdb699a677e19b966e32b234aa8
Veronica-Alfr <veagalves@gmail.com>
2022-03-09 20:30:37 -0300

    Requisito 7 em progresso

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getMusics } from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: '',
+      contentApi: [],
+      nameAlbum: '',
+      nameArtistOrBand: '',
+      saveArtistOrBand: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() { // a lógica pode ser inversa já que DidMount é após tudo que carregou.
+    const { id } = this.state; // observar match params -> visualização do código de Imar Mendes.
+    this.setState({
+      loading: false,
+    }, async () => {
+      const album = await getMusics(id); // pq não é lido?
+      this.setState({
+        contentApi: album,
+      });
+    });
+  }
+
   render() {
+    const { id, loading, nameAlbum, nameArtistOrBand,
+      saveArtistOrBand, contentApi } = this.state;
     return (
       <section>
         <Header />
